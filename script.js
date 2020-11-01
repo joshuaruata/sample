56cd6a3270dec55331e45b76388484111cd59ea7
joshuaruata <70530293+joshuaruata@users.noreply.github.com>
2020-11-01 14:59:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var inputBox = document.body.querySelector(".username");
+var messageBox = document.body.querySelector(".message");
+
+function makeNote(){
+  contentEle.innerHTML= document.body.querySelector("")
+}
+
+document.body.querySelector(".submit").addEventListener("click", function(){
+   if(inputBox.value=="coolStudent123"){
+     messageBox.innerHTML="Welcome!!"
+   }else{
+     messageBox.innerHTML="Wrong Username!!"
+   }
+})
